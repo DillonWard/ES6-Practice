c2fe45994602bce57be9e4cb8771985f46af2ffe
Dillon Ward <Dillonward13@gmail.com>
2018-09-19 10:40:57 +0200

    const example made

---
+++ js/constants.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+    // define a constant - a constant is a value that cannot be changed
+    const pi = 3.142;
+
+    // try to change the value of the const
+   // pi = 10;
+
+    // the output will throw an error because of the attempt to change the const
+    console.log(pi)
+
+    function calculateArea(r){
+
+        // pi cannot be changed, but it can be redefined
+       // const pi = 10;
+
+        // returns the area - pi * r(radius) squared
+        console.log("Area is: " + pi * (Math.pow(r, 2)));
+    }
+
+    calculateArea(5);
+}
